5d1d1f5b467e42b84cad62c5293007b3b29288af
Oscar de Caralt Roy <81873328+osciva@users.noreply.github.com>
2023-05-10 17:24:53 +0200

    update amb frontend del main.py
    part que no es pujava al fer git add .

--- main.py
+++ main.py
@@ -6,12 +6,31 @@ import repository, models, schemas
 from database import SessionLocal, engine
 from typing import List
 
+from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi import Request
+from fastapi.templating import Jinja2Templates
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+app.mount("/static", StaticFiles(directory="frontend/dist/static"), name="static")
+
+
+templates = Jinja2Templates(directory="frontend/dist")
 
 
 
 models.Base.metadata.create_all(bind=engine) # Creem la base de dades amb els models que hem definit a SQLAlchemy
-
-app = FastAPI()
 
 # Dependency to get a DB session
 def get_db():
